99f4b2a65b16443432bb27ebfbc99a5dbd4b14fb
Andy Short <automation.andy101@gmail.com>
2023-09-03 22:19:03 +0100

    Added PromptCard component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,59 @@
+"use client"
+import { useState, useEffect } from 'react'
+
+import PromptCard from './PromptCard'
+
+const PromptCardList = ({ data, handleTagClick}) => {
+  return (
+    <div className='mt-16 prompt_layout'>
+      {data.map((post) => (
+        <PromptCard
+          ket={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+
+    </div>
+  )
 
 const Feed = () => {
+  const [searchText, setSearchText ] = useState('')
+  const [posts, setPosts] = useState([])
+
+  const handleSearchChange = (e) => {
+
+  }
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json()
+      setPosts(data)
+    }
+
+    fetchPosts()
+  }, [])
+
   return (
-    <div>
-      Feed
-    </div>
+    <section>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or a username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input perr'
+        />
+      </form>
+
+      <PromptCardList 
+        data={posts}
+        handleTagClick={() => {}}
+      />
+
+    </section>
   )
 }
 
